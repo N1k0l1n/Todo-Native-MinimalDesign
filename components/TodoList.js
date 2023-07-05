3ee4424f83e6a466be6219c24ca488fc95919d9e
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-07-05 16:00:09 +0200

    update TodoList

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,13 +1,39 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 const TodoList = ({ todosData }) => {
+  const [todos, setTodos] = useState(todosData);
+
+  const toggleCheckbox = (index) => {
+    const updatedTodos = todos.map((todo, i) => {
+      if (i === index) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  };
+
   return (
     <View style={styles.container}>
-      {todosData.map((todo, index) => (
+      {todos.map((todo, index) => (
         <View key={index} style={styles.todoItem}>
+          <TouchableOpacity
+            onPress={() => toggleCheckbox(index)}
+            style={[
+              styles.checkbox,
+              todo.completed && styles.checkboxChecked,
+            ]}
+          >
+            {todo.completed && (
+              <Text style={styles.checkmark}>✓</Text>
+            )}
+          </TouchableOpacity>
           <Text style={styles.todoTitle}>{todo.title}</Text>
-          <Text style={styles.todoHour}>{todo.hour}</Text>
         </View>
       ))}
     </View>
@@ -19,17 +45,31 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   todoItem: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     marginBottom: 5,
+  },
+  checkbox: {
+    width: 24,
+    height: 24,
+    borderWidth: 2,
+    borderRadius: 4,
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  checkboxChecked: {
+    backgroundColor: '#000',
+    borderColor: '#000',
+  },
+  checkmark: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
   todoTitle: {
     fontSize: 16,
     marginRight: 10,
-  },
-  todoHour: {
-    fontSize: 14,
-    color: "gray",
   },
 });
 
