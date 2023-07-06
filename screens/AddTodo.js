90a902e885f6bcf9a048a40f3d3d2d2a744d0fb0
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-07-06 09:43:22 +0200

    fix AddTodo, stable

--- screens/AddTodo.js
+++ screens/AddTodo.js
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Switch, TouchableOpacity, TextInput } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet, TextInput, Switch, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { useDispatch, useSelector } from 'react-redux';
 import { addTodoReducer } from '../redux/todoSlice';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
+import * as Notifications from 'expo-notifications';
 
 const AddTodo = () => {
   const [name, setName] = useState('');
   const [date, setDate] = useState(new Date());
-  const [isToday, setIsToday] = useState(true);
+  const [isToday, setIsToday] = useState(false);
+  const [withAlert, setWithAlert] = useState(false);
+  const [showClock, setShowClock] = useState(false);
   const listTodos = useSelector(state => state.todos.todos);
-  const dispatch = useDispatch();
   const navigation = useNavigation();
-  const [selectedDate, setSelectedDate] = useState(null);
+  const dispatch = useDispatch();
 
   const addTodo = async () => {
     const newTodo = {
@@ -22,14 +24,37 @@ const AddTodo = () => {
       hour: date.toString(),
       isToday: isToday,
       isCompleted: false,
-    }
+    };
+
     try {
       await AsyncStorage.setItem("@Todos", JSON.stringify([...listTodos, newTodo]));
       dispatch(addTodoReducer(newTodo));
       console.log("Todos added successfully");
+
+      if (withAlert) {
+        scheduleTodoNotification(newTodo);
+      }
+
       navigation.goBack();
     } catch (err) {
       console.log(err);
+    }
+  };
+
+  const scheduleTodoNotification = async (todo) => {
+    const trigger = new Date(todo.hour);
+
+    try {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Alert! You have a task to do!',
+          body: todo.text,
+        },
+        trigger,
+      });
+      console.log('Notification scheduled');
+    } catch (e) {
+      alert('The notification failed to schedule. Make sure the hour is valid.');
     }
   };
 
@@ -41,76 +66,99 @@ const AddTodo = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Add Task</Text>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Name</Text>
-        <TextInput
-          style={styles.input}
-          value={name}
-          onChangeText={setName}
-          placeholder="Enter task name"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Hour</Text>
-        <TouchableOpacity style={styles.clockContainer} onPress={() => setSelectedDate(date)}>
-          <Text style={styles.clockText}>{date.toLocaleTimeString()}</Text>
-        </TouchableOpacity>
-        {selectedDate && (
-          <DateTimePicker
-            value={selectedDate}
-            mode="time"
-            display={Platform.OS === 'ios' ? 'spinner' : 'default'}
-            onChange={handleDateChange}
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Add a Task</Text>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputTitle}>Name</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Task"
+            placeholderTextColor="#00000030"
+            onChangeText={text => setName(text)}
           />
-        )}
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Today</Text>
-        <View style={styles.switchContainer}>
-          <Text style={styles.switchLabel}>No</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputTitle}>Hour</Text>
+          <TouchableOpacity style={styles.clockContainer} onPress={() => setShowClock(true)}>
+            <Text style={styles.clockText}>{date.toLocaleTimeString()}</Text>
+          </TouchableOpacity>
+          {showClock && (
+            <DateTimePicker
+              value={date}
+              mode="time"
+              display="default"
+              onChange={handleDateChange}
+            />
+          )}
+        </View>
+        <View style={[styles.inputContainer, { paddingBottom: 0, alignItems: 'center' }]}>
+          <View>
+            <Text style={styles.inputTitle}>Today</Text>
+            <Text style={{ color: '#00000040', fontSize: 12, maxWidth: '84%', paddingBottom: 10 }}>
+              If you disable today, the task will be considered astomorrow.
+            </Text>
+          </View>
           <Switch
-            style={styles.switch}
             value={isToday}
             onValueChange={value => setIsToday(value)}
-            trackColor={{ false: "#CCCCCC", true: "#CCCCCC" }}
-            thumbColor={isToday ? "#000000" : "#FFFFFF"}
           />
-          <Text style={styles.switchLabel}>Yes</Text>
         </View>
+        <View style={[styles.inputContainer, { paddingBottom: 0, alignItems: 'center' }]}>
+          <View>
+            <Text style={styles.inputTitle}>Alert</Text>
+            <Text style={{ color: '#00000040', fontSize: 12, maxWidth: '85%' }}>
+              You will receive an alert at the time you set for this reminder
+            </Text>
+          </View>
+          <Switch
+            value={withAlert}
+            onValueChange={value => setWithAlert(value)}
+          />
+        </View>
+        <TouchableOpacity onPress={addTodo} style={styles.button}>
+          <Text style={{ color: 'white' }}>Done</Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.button} onPress={addTodo}>
-        <Text style={styles.buttonText}>Done</Text>
-      </TouchableOpacity>
-      <Text style={styles.infoText}>
-        If you disable today, the task will be considered as tomorrow.
-      </Text>
-    </View>
+    </TouchableWithoutFeedback>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 34,
+    fontWeight: 'bold',
+    marginBottom: 35,
+    marginTop: 10,
+  },
+  textInput: {
+    borderBottomColor: '#00000030',
+    borderBottomWidth: 1,
+    width: '80%',
+  },
   container: {
     flex: 1,
     backgroundColor: '#F7F8FA',
-    padding: 30,
+    paddingHorizontal: 30,
   },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    marginBottom: 25,
+  inputTitle: {
+    fontSize: 20,
+    fontWeight: '600',
+    lineHeight: 24,
   },
   inputContainer: {
-    marginBottom: 20,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingBottom: 30,
   },
-  label: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
-  input: {
-    borderColor: '#CCCCCC',
+  button: {
+    marginTop: 30,
+    marginBottom: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000000',
+    height: 46,
+    borderRadius: 11,
   },
   clockContainer: {
     flexDirection: 'row',
@@ -120,33 +168,6 @@ const styles = StyleSheet.create({
   clockText: {
     fontSize: 16,
     marginRight: 10,
-  },
-  switchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  switchLabel: {
-    fontSize: 16,
-    marginRight: 10,
-  },
-  switch: {
-    transform: [{ scaleX: 0.9 }, { scaleY: 0.9 }],
-    marginRight: 10,
-  },
-  button: {
-    backgroundColor: '#000000',
-    borderRadius: 8,
-    paddingVertical: 12,
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-  },
-  infoText: {
-    color: '#00000060',
-    marginTop: 20,
   },
 });
 
